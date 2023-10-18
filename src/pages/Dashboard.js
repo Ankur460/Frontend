7ee55bc881da3f0823695558825d0ed2bdab42eb
Ankur460 <ankurrai460@gmail.com>
2023-10-18 14:17:39 +0530

    Second commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
 
 function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Stack sx={{width:1,height:'100%'}}>
+        <img src='https://www.physiospot.com/wp-content/uploads/2015/09/intro-dashboard-overview.png'></img>
+    </Stack>
   )
 }
 
